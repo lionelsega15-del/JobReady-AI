7d3e2cbe5247f261da97278509e623bf6c0f98c7
lionelsega15-del <lionelsega15@gmail.com>
2026-09-14 20:24:50 +0700

    fix: remove invalid module augmentation in types

--- src/types/index.ts
+++ src/types/index.ts
@@ -111,18 +111,3 @@ export interface ColorblindTestResult {
   recommendation: string;
 }
 
-declare module '*.jpg' {
-  const src: string;
-  export default src;
-}
-
-declare module '*.png' {
-  const src: string;
-  export default src;
-}
-
-declare module '*.svg' {
-  const src: string;
-  export default src;
-}
-
